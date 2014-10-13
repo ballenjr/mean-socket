58b388d1a7e72c6bfd8a61c85a9369e1d4d64ef7
Brent Allen <ballen@eaglemail.csi.edu>
2014-10-13 01:06:02 -0600

    manglable syntax

--- public/services/meanSocket.js
+++ public/services/meanSocket.js
@@ -2,27 +2,28 @@
 
 var baseUrl = 'http://' + location.hostname + ':89/';
 
-angular.module('mean.mean-socket').factory('MeanSocket', function($rootScope) {
-	var socket = io.connect(baseUrl);
-	return {
-		on: function(eventName, callback) {
-			socket.on(eventName, function() {
-				var args = arguments;
-				$rootScope.$apply(function() {
-					callback.apply(socket, args);
-				});
-			});
-		},
-		emit: function(eventName, data, callback) {
-			socket.emit(eventName, data, function() {
-				console.log('event:', eventName);
-				var args = arguments;
-				$rootScope.$apply(function() {
-					if (callback) {
-						callback.apply(socket, args);
-					}
-				});
-			});
-		}
-	};
-});
+angular.module('mean.mean-socket').factory('MeanSocket', ['$rootScope', function($rootScope) {
+    var socket = io.connect(baseUrl);
+    var ws = {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, data, function() {
+                console.log('event:', eventName);
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            });
+        }
+    };
+    return ws;
+}]);
